397c4af1f585b6c9840ae324852fab2530dfc977
Lokeshrajkp <kplokeshraj17@gmail.com>
2022-12-07 09:32:07 +0530

    update

--- data_dump.py
+++ data_dump.py
@@ -2,7 +2,9 @@ import pymongo
 import pandas as pd
 import json
 # Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+client = pymongo.MongoClient("
+mongodb+srv://Lokesh100:ind100meC@cluster0.gb2w07a.mongodb.net/?retryWrites=true&w=majority
+")
 
 DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
 DATABASE_NAME="aps"
